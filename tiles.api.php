99d68a5113f4b19fcb13dc0d8871186f85540580
Martin Rio <martin@ombuweb.com>
2012-11-02 19:16:07 -0700

    tiles.api.php: adds some hook documentation

--- tiles.api.php
+++ tiles.api.php
@@ -1,9 +1,24 @@
 <?php
 
-function hook_ombublock_info() {
-  // naming convention is MODULE__DELTA
+/**
+ * @file
+ * Hooks provided by the Tiles module.
+ */
+
+/**
+ * Tell Tiles which blocks it should manage.
+ */
+function hook_tiles_info(&$tiles) {
+  // Naming convention is MODULE__DELTA.
   $tiles['user__new'] = array(
     'regions' => array('foo', 'bar', 'baz'),
   );
   return $tiles;
+}
+
+/**
+ * Tell Tiles which regions it can manage blocks in.
+ */
+function hook_tiles_regions(&$regions) {
+  return array('content');
 }
